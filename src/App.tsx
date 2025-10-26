69b8bd78a311d28dfa4a5ab61122a45772009140
96mdjanealam <96mdjanealam@gmail.com>
2025-10-26 23:35:41 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+     <h1>hello world</h1>
+     <Button>click</Button>
+    </>
+  )
+}
+
+export default App
